ec1749a3d5e0ea3cccd0c9be21cb9fd06038ce04
Keyur Barot <71115771+jagruti-technology@users.noreply.github.com>
2026-03-06 23:20:53 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,26 +1,12 @@
-// smooth scroll
-
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
 anchor.addEventListener('click', function (e) {
 
-e.preventDefault();
+e.preventDefault()
 
-document.querySelector(this.getAttribute('href')).scrollIntoView({
-behavior: 'smooth'
-});
+document.querySelector(this.getAttribute('href'))
+.scrollIntoView({behavior:'smooth'})
 
-});
+})
 
-});
-
-
-// contact form confirmation
-
-const form = document.getElementById("contact-form");
-
-form.addEventListener("submit", function(){
-
-alert("Thanks for contacting Jagruti Technology! We will reach out soon.");
-
-});
+})
