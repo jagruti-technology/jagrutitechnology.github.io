dc8bc11f3b2860644f97d763d8a68c7bb9d3cb35
Keyur Barot <71115771+jagruti-technology@users.noreply.github.com>
2026-03-07 18:54:22 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
 anchor.addEventListener('click', function (e) {
 
-e.preventDefault()
+e.preventDefault();
 
 document.querySelector(this.getAttribute('href'))
-.scrollIntoView({behavior:'smooth'})
+.scrollIntoView({behavior: 'smooth'});
 
-})
+});
 
-})
+});
